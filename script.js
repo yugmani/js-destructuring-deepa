b9b4ea83f25a984b562101c2320ec0c247febcdb
yugmani <yugmani@yahoo.com>
2022-06-29 11:15:56 -0700

    assign default values

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Destructuring in Arrays
+// ******************************
 
 const [name1, name2, ...name3] = ['Andrew', 'Justin', 'Katherine', 'Jessica'];
 
@@ -12,3 +13,19 @@ const [a, , , ...c] = [1, 2, 3, 4, 5, 6];
 console.log('a: ', a); // a: 1
 console.log('c: ', c); // c: [4, 5, 6]
 //array elements 2,3 are skipped
+
+//Assign default value while Destructuring
+// *****************************************
+
+const [name, street = 'Beverly Hills', age = 'Minor', phone] = [
+  'Yoog',
+  ,
+  ,
+  undefined,
+  null,
+];
+
+console.log(name); //Yoog
+console.log(street); //Beverly Hills  ?
+console.log(age); //Minor ?
+console.log(phone); //undefined
